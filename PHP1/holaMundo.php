53472f8a0a7dd05cef19e5781c3bf325f6e50477
Victor Garcia <victor.gsa9@gmail.com>
2021-10-27 12:10:17 +0200

    añadido en clase

---
+++ PHP1/holaMundo.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Hola Mundo</title>
+    </head>
+    <body>
+      <?php
+      $nombre = "Víctor García";
+      echo "¡Hola Mundo!";
+      echo "<br>";
+      echo "Soy" ." " . $nombre;
+      echo "<br>";
+      echo "Esta es mi primera web dinámica"
+      ?>  
+        
+    </body>
+</html>
+
+
+
---
+++ PHP1/prueba.php
@@ -0,0 +1,21 @@
+<?php
+
+$ext="PDF";
+
+switch ($ext)
+{   
+    
+    case("PDF"):
+    $tipo = "Documento Adobe PDF";
+    #break;
+        
+    case("TXT"):
+    $tipo = "Documento TXT";
+    #break;
+    
+   
+
+
+}
+
+echo $tipo;
