197688da7b474786625dacf653cf995b08ea9894
Victor Garcia <victor.gsa9@gmail.com>
2021-12-01 09:29:41 +0100

    Terminado

--- PHP2IntroForm/operac1_4.php
+++ PHP2IntroForm/operac1_4.php
@@ -1,8 +1,8 @@
 <?php
 
 //Asignación de valores a variables
-$n1=$_GET["operador1"];
-$n2=$_GET["operador2"];
+$n1=$_GET['operador1'];
+$n2=$_GET['operador2'];
 
 echo "<h1>Introducción de dos números</h1>";
 echo "<h3>Operaciones básicas</h3>";
@@ -14,7 +14,7 @@ $mult=$n1*$n2;
 $div=$n1/$n2;
 $mod=$n1%$n2;
 
-echo "<h1>Las operaciones aritméticas son:</h1>";
+echo "<h2>Las operaciones aritméticas son:</h2>";
 echo "La suma vale: $suma<br/>";
 echo "La resta vale: $resta<br/>";
 echo "La multiplicación vale: $mult<br/>";
@@ -23,9 +23,26 @@ echo "El resto de la división vale: $mod<p/>";
 
 //Operaciones lógicas
 
-echo "<h1>Las operaciones LÓGICAS son</h1>";
+echo "<h2>Las operaciones LÓGICAS son</h2>";
+if ($n1==true && $n2==true){
+    echo "AND de los números: " .true ."<br/>";
+}
+if ($n1==true || $n2==true){
+    echo "OR de los números: " .true ."<br/>";
+}
 
 
 //Operaciones a nivel de BIT
-echo "<h1>Las operaciones a nivel de BIT son</h1>";
 
+$and=$n1&$n2;
+$or=$n1|$n2;
+$izqda=$n1 << $n2;
+$drcha=$n1 >> $n2;
+
+
+echo "<h2>Las operaciones a nivel de BIT son</h2>";
+
+echo "AND bit de los números: $and<br/>";
+echo "OR bit de los números: $or<br/>";
+echo "Desplazamiento del número $n1 ==> $n2 posiciones a la izqda $izqda<br/>";
+echo "Desplazamiento del número $n1 ==> $n2 posiciones a la drcha $drcha";
---
+++ PHP2IntroForm/operac1_6.php
@@ -0,0 +1,57 @@
+<?php
+
+//Asignación de variables
+$n1=$_GET['operador1'];
+$n2=$_GET['operador2'];
+
+if (empty($n1) && empty($n2)){
+    echo "<b>Ambos campos están vacíos</b>";
+}
+elseif (empty ($n1)){
+    echo "<b>El primer campo está vacío</b>";
+}
+elseif (empty ($n2)){
+    echo "<b>El segundo campo está vacío</b>";
+}
+else {
+    //Operaciones aritméticas
+    $suma=$n1+$n2;
+    $resta=$n1-$n2;
+    $mult=$n1*$n2;
+    $div=$n1/$n2;
+    $mod=$n1%$n2;
+
+    echo "<h2>Las operaciones aritméticas son:</h2>";
+    echo "La suma vale: $suma<br/>";
+    echo "La resta vale: $resta<br/>";
+    echo "La multiplicación vale: $mult<br/>";
+    echo "La división vale: $div<br/>";
+    echo "El resto de la división vale: $mod<p/>";
+
+    //Operaciones lógicas
+
+    echo "<h2>Las operaciones LÓGICAS son</h2>";
+    if ($n1==true && $n2==true){
+        echo "AND de los números: " .true ."<br/>";
+    }
+    if ($n1==true || $n2==true){
+        echo "OR de los números: " .true ."<br/>";
+    }
+
+
+    //Operaciones a nivel de BIT
+
+    $and=$n1&$n2;
+    $or=$n1|$n2;
+    $izqda=$n1 << $n2;
+    $drcha=$n1 >> $n2;
+
+    echo "<h2>Las operaciones a nivel de BIT son</h2>";
+
+    echo "AND bit de los números: $and<br/>";
+    echo "OR bit de los números: $or<br/>";
+    echo "Desplazamiento del número $n1 ==> $n2 posiciones a la izqda $izqda<br/>";
+    echo "Desplazamiento del número $n1 ==> $n2 posiciones a la drcha $drcha";
+    
+}
+
